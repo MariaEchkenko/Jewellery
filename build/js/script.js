c16f41b2de2276cc9145f475911e7f40d45de07e
Maria Echkenko <mashasly@mail.ru>
2021-09-18 15:40:39 +0300

    Реализована шапка

---
+++ build/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+var header = document.querySelector('.page-header__wrapper');
+var navMain = document.querySelector('.page-header__nav');
+var navToggle = document.querySelector('.page-header__nav-toggle');
+
+if (navToggle) {
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('page-header__nav--closed')) {
+      navMain.classList.remove('page-header__nav--closed');
+      navMain.classList.add('page-header__nav--opened');
+      header.classList.add('page-header__wrapper--active');
+      document.body.classList.add('no-scroll');
+    } else {
+      navMain.classList.add('page-header__nav--closed');
+      navMain.classList.remove('page-header__nav--opened');
+      header.classList.remove('page-header__wrapper--active');
+      document.body.classList.remove('no-scroll');
+    }
+  });
+}
